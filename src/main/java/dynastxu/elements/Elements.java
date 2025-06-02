4fef9c01880a484d19b6321b197266ffd3df317f
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2025-06-02 21:53:26 +0800

    feat(elements): 添加元素枚举类
    - 创建 Elements 枚举类，定义了原神游戏中的七种元素

---
+++ src/main/java/dynastxu/elements/Elements.java
@@ -0,0 +1,12 @@
+package dynastxu.elements;
+
+public enum Elements {
+    NONE,
+    Pyro, // 火元素
+    Hydro, // 水元素
+    Anemo, // 风元素
+    Electro, // 雷元素
+    Dendro, // 草元素
+    Cryo, // 冰元素
+    Geo // 岩元素
+}

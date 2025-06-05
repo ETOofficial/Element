aa1936e5c84da276781ba6b0f2faca87c82dfc8a
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2025-06-05 10:52:16 +0800

    feat(network): 实现客户端网络通信功能
    - 新增 NetworkClient 类，用于处理客户端网络通信
    - 实现元素同步和元素反应的接收功能
    - 注册全局接收器和连接事件监听器
    - 优化数据处理和实体查找逻辑

---
+++ src/client/java/dynastxu/network/NetworkClient.java
@@ -0,0 +1,77 @@
+package dynastxu.network;
+
+import dynastxu.api.ILivingEntityData;
+import dynastxu.elements.AttachedElement;
+import dynastxu.elements.Reactions;
+import dynastxu.event.ReactionEventClient;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static dynastxu.network.Network.ELEMENT_REACTION_CHANNEL;
+import static dynastxu.network.Network.ELEMENT_SYNC_CHANNEL;
+
+public class NetworkClient {
+    public static void init() {
+        elementSynchronousReceptionRegister();
+        elementalReactionReceivingRegister();
+    }
+
+    private static void elementSynchronousReceptionRegister() {
+        // 元素同步接收
+        ClientPlayNetworking.registerGlobalReceiver(
+                ELEMENT_SYNC_CHANNEL,
+                (client, handler, buf, responseSender) -> {
+                    int entityId = buf.readVarInt();
+                    NbtList list = buf.readNbt().getList("elements", NbtElement.COMPOUND_TYPE);
+
+                    client.execute(() -> {
+                        ClientWorld world = client.world;
+                        if (world != null) {
+                            Entity entity = world.getEntityById(entityId);
+                            if (entity instanceof LivingEntity) {
+                                ILivingEntityData data = (ILivingEntityData) entity;
+                                List<AttachedElement> elements = new ArrayList<>();
+
+                                for (int i = 0; i < list.size(); i++) {
+                                    elements.add(AttachedElement.deserialize(list.getCompound(i)));
+                                }
+
+                                data.element$setAttachedElements(elements);
+                            }
+                        }
+                    });
+                }
+        );
+    }
+
+    private static void elementalReactionReceivingRegister() {
+        // 元素反应接收
+        ClientPlayConnectionEvents.JOIN.register((handler1, sender, client1) -> {
+            ClientPlayNetworking.registerReceiver(ELEMENT_REACTION_CHANNEL, (client, handler, buf, responseSender) -> {
+                int entityId = buf.readVarInt();
+                Reactions reaction = buf.readEnumConstant(Reactions.class);
+
+                client.execute(() -> {
+                    Entity entity = client.world.getEntityById(entityId);
+                    if (entity != null) {
+                        switch (reaction) {
+                            case Vaporize -> ReactionEventClient.vaporateEvent(client, entity);
+                            case Melt -> ReactionEventClient.meltEvent(client, entity);
+                            case Overload -> ReactionEventClient.overloadEvent(client, entity);
+                        }
+                    }
+                });
+            });
+        });
+    }
+
+
+}
